2b4bb0df7a67db92422263849ed7bc8d173518ef
Odhran Sexton <119361266@umail.ucc.ie>
2022-04-03 02:10:42 +0100

    The bot now opens the group chat I want it to open

--- InstaBot.py
+++ InstaBot.py
@@ -1,3 +1,4 @@
+from cgi import test
 from time import sleep
 from selenium import webdriver
 
@@ -21,8 +22,8 @@ class InstaBot:
         username_input = self.browser.find_element(by="css selector", value="input[name='username']")
         password_input = self.browser.find_element(by="css selector", value="input[name='password']")
 
-        username_input.send_keys("<Your username>")
-        password_input.send_keys("<Your Password>")
+        username_input.send_keys("")
+        password_input.send_keys("")
 
         login_button = self.browser.find_element(by="xpath", value="//button[@type='submit']")
         login_button.click()
@@ -32,8 +33,30 @@ class InstaBot:
         save_details_button = self.browser.find_element(by="xpath", value="//button[text()='Not now']")
         save_details_button.click()
 
-        sleep(10)
+        sleep(5)
+
+        save_details_button = self.browser.find_element(by="xpath", value="/html/body/div[6]/div/div/div/div[3]/button[2]")
+        save_details_button.click()
+
+        sleep(5)
+
+    def open_messages(self):
+
+        messages_button = self.browser.find_element(by="xpath", value="/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[2]/a")
+        messages_button.click()
+
+        sleep(5)
+
+        dbdChat_button = self.browser.find_element(by="xpath", value="/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[2]/div/div/div/div/div[2]/a")
+        dbdChat_button.click()
+
+        sleep(5)
 
     def close_browser(self):
 
         self.browser.close()
+
+if __name__ == "__main__":
+    testBot = InstaBot()
+    testBot.login()
+    testBot.open_messages()
