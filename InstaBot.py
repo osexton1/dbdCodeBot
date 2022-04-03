7df14e290481c8eabd880086394e6696aca0b060
Odhran Sexton <119361266@umail.ucc.ie>
2022-04-03 01:49:09 +0100

    Broke the code into separate functions

--- InstaBot.py
+++ InstaBot.py
@@ -2,34 +2,38 @@ from time import sleep
 from selenium import webdriver
 
 class InstaBot:
-    def __init__():
+    def __init__(self):
 
-        browser = webdriver.Firefox()
-        browser.implicitly_wait(5)
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
 
-        browser.get('https://instagram.com')
+        self.browser.get('https://instagram.com')
 
         sleep(2)
 
-        cookie_button = browser.find_element(by="xpath", value="//*[text()='Only Allow Essential Cookies']")
+        cookie_button = self.browser.find_element(by="xpath", value="//*[text()='Only Allow Essential Cookies']")
         cookie_button.click()
 
         sleep(5)
 
-        username_input = browser.find_element(by="css selector", value="input[name='username']")
-        password_input = browser.find_element(by="css selector", value="input[name='password']")
+    def login(self):
+
+        username_input = self.browser.find_element(by="css selector", value="input[name='username']")
+        password_input = self.browser.find_element(by="css selector", value="input[name='password']")
 
         username_input.send_keys("<Your username>")
         password_input.send_keys("<Your Password>")
 
-        login_button = browser.find_element(by="xpath", value="//button[@type='submit']")
+        login_button = self.browser.find_element(by="xpath", value="//button[@type='submit']")
         login_button.click()
 
         sleep(5)
 
-        save_details_button = browser.find_element(by="xpath", value="//button[text()='Not now']")
+        save_details_button = self.browser.find_element(by="xpath", value="//button[text()='Not now']")
         save_details_button.click()
 
         sleep(10)
 
-        browser.close()
+    def close_browser(self):
+
+        self.browser.close()
